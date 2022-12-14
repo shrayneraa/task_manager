7826666ad38b1997a78f9c6f665cd97142351787
Artem Shrayner <shrayner.aa@yandex.ru>
2022-12-14 21:54:41 +0500

    third commit

--- yatube/core/context_processors/year.py
+++
@@ -1,8 +0,0 @@
-from django.utils import timezone
-
-
-def year(request):
-
-    return {
-        'year': timezone.datetime.now().year,
-    }
--- yatube/posts/utils.py
+++
@@ -1,9 +0,0 @@
-from django.core.paginator import Paginator
-from core.constants import POSTS_PER_PAGE
-
-
-def util_paginator(request, post_list):
-    paginator = Paginator(post_list, POSTS_PER_PAGE)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    return page_obj
